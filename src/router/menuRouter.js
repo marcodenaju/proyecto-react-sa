b4ae9443e2a3ba8895911be67414da1bcd0adbb1
Nahuel D <marco.denaju@gmail.com>
2023-11-18 14:06:08 -0300

    se eliminaron carpetas sin uso  y comentarios

--- src/router/menuRouter.js
+++ src/router/menuRouter.js
@@ -1,10 +1,7 @@
 import Cart from "../components/pages/cart/Cart";
 import ItemDetailContainer from "../components/pages/itemDetailContainer/ItemDetailContainer";
 import ItemListContainer from "../components/pages/itemListContainer/ItemListContainer";
-// import Checkout from "../components/pages/checkout/Checkout.jsx";
-// import CheckoutFormik from "../components/pages/checkoutFormik/CheckoutFormik";
 import CheckoutOficial from "../components/pages/checkoutOficial/CheckoutOficial";
-
 
 export const routes = [
   {
